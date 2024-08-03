8cccf03d43b85ff3eeaf44cb4de49362592be2bc
ianlee08 <ileejam123@gmail.com>
2024-08-03 12:23:47 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div className = "flex-container">
+        <div className = "flex-items">
+          <div className = "flex-item-container flex-item-1"> 
+            A
+          </div>
+          <div className = "flex-item-name">Alice</div>
+        </div>
+        <div className = "flex-items">
+          <div className = "flex-item-container flex-item-2">
+            B
+          </div>
+          <div className = "flex-item-name">Bob</div>
+        </div>
+        <div className = "flex-items">
+          <div className = "flex-item-container flex-item-3">
+            C
+          </div>
+          <div className = "flex-item-name">Chars</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
